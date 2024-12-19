1d94816111f7894c00773a936d862c1597044220
AlejaandroRubio <alejandro.rubio@alumnos.uneatlantico.es>
2024-12-19 14:11:53 +0100

    Cambios de .env

--- backend/src/services/omdService.ts
+++ backend/src/services/omdService.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
-//const OMD_API_KEY = process.env.OMDB_API_KEY;
 const OMD_API_KEY = process.env.OMDB_API_KEY;
-const OMDB_BASE_URL = process.env.OMDB_BASE_URL;
+const OMDB_BASE_URL = 'http://www.omdbapi.com/'
 
 export const getMoviesByTitle = async (query: string) => {
 
     const ApiFullUrl = `${OMDB_BASE_URL}?apikey=${OMD_API_KEY}&t=${query}`;
+    console.log(ApiFullUrl);
     const response = await axios.get(ApiFullUrl);
     let movies = response.data;
     return movies;
